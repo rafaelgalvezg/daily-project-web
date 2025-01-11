f49a546222ccb4065933e898f4dd644324e3bc97
rafaelGalvezG <rafael.galvezg@gmail.com>
2025-01-11 15:19:42 -0600

    add environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://localhost:9090/daily-project',
+  TOKEN_NAME: 'access_token',
+  HTTP_RETRY: 2
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  apiBaseUrl: 'http://localhost:9090/daily-project',
+  TOKEN_NAME: 'access_token',
+  HTTP_RETRY: 2
+};
